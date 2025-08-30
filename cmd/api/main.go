3c8cdc4ec1666990ab7a4fc2feaf4788a0380544
Daniel Vale <danielsvba@hotmail.com>
2025-08-29 22:06:07 -0300

    feat: Create a main function to initialize the product manager server

---
+++ cmd/api/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	fmt.Println("Iniciando o servidor da API de produtos")
+
+	logger := log.New(os.Stdout, "product-manager-API: ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	err := godotenv.Load()
+
+	if err != nil {
+		logger.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+	}
+
+	addr := os.Getenv("DB_ADDR")
+
+	if addr == "" {
+		addr = ":3000"
+	}
+
+	logger.Println("Conectando ao banco de dados...")
+
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	))
+
+	if err != nil {
+		logger.Fatalf("Erro ao abrir a conexão com o banco de dados: %v", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		logger.Fatalf("Não foi possível verificar a conexão com o banco de dados: %v", err)
+	}
+
+	logger.Println("Conexão com o banco de dados estabelecida com sucesso.")
+
+	srv := &http.Server{
+		Addr:         addr,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	logger.Printf("Iniciando o servidor na porta %s", addr)
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Não foi possível iniciar o servidor: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/valedaniel/golang-product-manager
+
+go 1.25.0
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
